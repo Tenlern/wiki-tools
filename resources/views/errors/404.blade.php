05672fa6b8e6f931bd60bb0555a1063a7fa0b92a
IlyaL <illyash200009@ya.ru>
2023-01-16 00:00:40 +0300

    Make 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,10 +10,9 @@
              fill="currentColor"
              viewBox="0 0 20 20"
         >
-            <path
-                d="M13.477 14.89A6 6 0 015.11 6.524l8.367 8.368zm1.414-1.414L6.524 5.11a6 6 0 018.367 8.367zM18 10a8 8 0 11-16 0 8 8 0 0116 0z"
-                fill-rule="evenodd"
-                clip-rule="evenodd">
+            <path d="M13.477 14.89A6 6 0 015.11 6.524l8.367 8.368zm1.414-1.414L6.524 5.11a6 6 0 018.367 8.367zM18 10a8 8 0 11-16 0 8 8 0 0116 0z"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd">
             </path>
         </svg>
         <h1 class="text-6xl font-semibold text-gray-700 dark:text-gray-200">
